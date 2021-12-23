12aa878b92a99aa15f8544213c3c7ff9d705f13b
John Henderson <john.henderson728@gmail.com>
2021-12-23 18:02:09 +1000

    clarified some comments and explinations in relations to pointers

--- application/useful_pointers.c
+++ application/useful_pointers.c
@@ -16,18 +16,29 @@ int main(void) {
 
     // Lets say that we want to create a function that when we call it it returns a string
     // to us of a name. Before knowing pointers this was not possible as you could not return
-    // arrays. THe reason for this is when a program is compiled, C must know how large a return
-    // type needs to be. An array, unlike an int, has an unknown size to the compiler. Pointers
-    // on the other hand are known to have a set size. This means that we can create the array,
-    // then return a pointer to it.
+    // arrays from a function. The reason for this is when a program is compiled, C must know 
+    // how large a return type needs to be. An array, unlike an int, has an unknown size to the 
+    // compiler. Pointers on the other hand are known to have a set size. This means that we can 
+    // create the array, then return a pointer to it.
 
-    // Be careful, it takes a bit more effort than that.
+    // Be careful, it takes a bit more effort than that though.
 
     char *name_1 = return_name_poorly();
     printf("name_1 = %s\n", name_1);
 
     char *name_2 = return_name();
     printf("name_2 = %s\n", name_2);
+
+    // telling the computer to free the held memory
+    free(name_2);
+}
+
+// The array name will not exist outside of this function. Once this function ends, the 
+// memory will no longer exist. That means when we return the pointer, it cannot point
+// anywhere has the memory that holds the name has been destroyed.
+char *return_name_poorly() {
+    char name[] = "John Henderson";
+    return name;
 }
 
 // This function specifically tells C to not destroy 15 bytes of memory when this function ends.
@@ -36,13 +47,5 @@ int main(void) {
 char *return_name() {
     char *name = malloc(sizeof(char) * 15);
     memcpy(name, "John Henderson", 15);
-    return name;
-}
-
-// The array name will not exist outside of this function. Once this function ends, the 
-// memory will no longer exist. That means when we return the pointer, it cannot point
-// anywhere has the memory that holds the name has been destroyed.
-char *return_name_poorly() {
-    char name[] = "John Henderson";
     return name;
 }
